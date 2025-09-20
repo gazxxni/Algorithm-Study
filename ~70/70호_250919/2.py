f4f3f7473dc808e1471f4735a52989b930ac103b
gazxxni <jungsj0524@gmail.com>
2025-09-20 17:27:18 +0900

    a

--- ~70/70호_250919/2.py
+++ ~70/70호_250919/2.py
@@ -5,24 +5,13 @@ a = int(input())
 x = int(input())
 MOD = 1_000_000_007
 
-arr = [0]
-brr = [1]
-num = a
-b = 1
-for _ in range(64):
-    num **= 2
-    arr.append(num)
-    b *= 2
-    brr.append(b)
+result = 1
+a %= MOD
 
-ans = 1
-arr.sort(reverse=True)
-brr.sort(reverse=True)
-
-for i in range(len(brr)):
-    if x - brr[i] > 0:
-        x -= brr[i]
-
-        ans *= arr[i + 1]
+while x > 0:
+    if x % 2 == 1: 
+       result = (result * a) % MOD
+    a = (a * a) % MOD
+    x //= 2
     
-print(ans)
+print(result % MOD)
