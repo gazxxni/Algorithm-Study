ae1bf4708c19061bc7303e9ae3d8194a4fdbbb45
gazxxni <jungsj0524@gmail.com>
2025-10-11 18:03:34 +0900

    a

---
+++ ~80/72회_251010/3.py
@@ -0,0 +1,41 @@
+k = int(input())
+
+cnt = 0
+a = 1
+while True:
+  if a >= k:
+      break
+  
+  a *= 2
+  cnt += 1   
+
+if k % 2 == 1:
+    print(a, cnt)
+    
+elif k == a:
+    print(a, 0)
+    
+else:
+    cnt2 = 0
+    ans = a
+    while k > 0:
+        if k >= a:
+            k -= a
+        else:
+            a //= 2
+            cnt2 += 1
+    print(ans, cnt2)
+    
+
+
+
+
+
+
+
+
+# def bitcount(n):
+#     if n == 0:
+#         return 0
+    
+#     return n % 2 + bitcount(n // 2)
